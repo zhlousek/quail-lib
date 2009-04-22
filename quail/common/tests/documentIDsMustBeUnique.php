6d36ce819cf0c8741b3878b5d7cf4fd0bb8e17bf
keveemiller <keveemiller@0cbe4d32-2880-11de-b161-0f7b0ef88f65>
2009-04-22 17:24:21 +0000

    Finished test 185. Added xpath query.

--- quail/common/tests/documentIDsMustBeUnique.php
+++ quail/common/tests/documentIDsMustBeUnique.php
@@ -3,6 +3,15 @@
 class documentIDsMustBeUnique extends quailTest {
 	
 	function check() {
-		
+		$xpath = new DOMXPath($this->dom);
+		$entries = $xpath->query('//*');
+		foreach($entries as $element) {
+			if($element->hasAttribute('id'))
+				$ids[$element->getAttribute('id')][] = $element;
+		}	
+		foreach($ids as $id) {
+			if(count($id) > 1)
+				$this->addReport($id[1]);
+		}
 	}
 }
