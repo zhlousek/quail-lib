9553cbed2bb8e89a9692696e692a58d1b4393b7c
keveemiller <keveemiller@0cbe4d32-2880-11de-b161-0f7b0ef88f65>
2009-09-10 22:09:41 +0000

    Fixed foreach issue in document tests

--- trunk/quail/common/tests/linkUsedToDescribeNavigation.php
+++ trunk/quail/common/tests/linkUsedToDescribeNavigation.php
@@ -8,10 +8,12 @@ class linkUsedToDescribeNavigation extends quailTest {
 	function check() {
 		$head = $this->getAllElements('head');
 		$head = $head[0];
-		foreach($head->childNodes as $child) {
-			if($child->tagName == 'link' && $child->getAttribute('rel') != 'stylesheet')
-				return true;
+		if($head->childNodes) {
+			foreach($head->childNodes as $child) {
+				if($child->tagName == 'link' && $child->getAttribute('rel') != 'stylesheet')
+					return true;
+			}
+			$this->addReport(null, null, false);
 		}
-		$this->addReport(null, null, false);
 	}
 }
