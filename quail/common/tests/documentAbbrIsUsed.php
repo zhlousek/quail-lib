2f02c5a4332898c6242f9d1ed4e4379877778fb6
keveemiller <keveemiller@0cbe4d32-2880-11de-b161-0f7b0ef88f65>
2009-10-18 16:45:41 +0000

    Changed acronym tests to only report one error per un abbr acronym. Cleaned up how quail handled requiring test files

--- quail/common/tests/documentAbbrIsUsed.php
+++ quail/common/tests/documentAbbrIsUsed.php
@@ -8,16 +8,21 @@ class documentAbbrIsUsed extends quailTest {
 	
 	function check() {
 		foreach($this->getAllElements($this->acronym_tag) as $acronym) {
-			$predefined[trim($acronym->nodeValue)] = $acronym->getAttribute('title');
+			$predefined[strtoupper(trim($acronym->nodeValue))] = $acronym->getAttribute('title');
 		}
+		$already_reported = array();
 		foreach($this->getAllElements(null, 'text') as $text) {
 
 			$words = explode(' ', $text->nodeValue);
 			if(count($words) > 1 && strtoupper($text->nodeValue) != $text->nodeValue) {
 				foreach($words as $word) {
-					$word = preg_replace("/[^a-zA-Z0-9s]/", "", $word);
-					if(strtoupper($word) == $word && strlen($word) > 1 && !$predefined[$word])
-						$this->addReport($text);
+					$word = preg_replace("/[^a-zA-Zs]/", "", $word);
+					if(strtoupper($word) == $word && strlen($word) > 1 && !$predefined[strtoupper($word)])
+
+						if(!$already_reported[strtoupper($word)]) {
+							$this->addReport($text, 'Word "'. $word .'" requires an <code>'. $this->acronym_tag .'</code> tag.');
+						}
+						$already_reported[strtoupper($word)] = true;
 				}
 			}
 		}
