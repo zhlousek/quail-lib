2d20d5b7244c9f4e230cc38f383b28f3b98f4391
keveemiller <keveemiller@0cbe4d32-2880-11de-b161-0f7b0ef88f65>
2009-11-14 21:07:43 +0000

    Moved translation layer to the guideline (makes more sense that way)

--- quail/quail.php
+++ quail/quail.php
@@ -592,7 +592,10 @@ class quailGuideline {
 	*	Iteates through each test string, makes a new test object, and runs it against the current DOM
 	*/
 	function run($arg = null) {
-		foreach($this->tests as $testname) {
+		foreach($this->tests as $testname => $options) {
+			if(is_numeric($testname) && !is_array($options)) {
+				$testname = $options;
+			}
 			if(class_exists($testname) && $this->dom) {
 				$$testname = new $testname($this->dom, $this->css, $this->path);
 				$this->report[$testname] = $$testname->getReport();	
