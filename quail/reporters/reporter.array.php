068f7d292ef4b4eb571a969f25ae97bb8a11a095
keveemiller <keveemiller@0cbe4d32-2880-11de-b161-0f7b0ef88f65>
2010-04-11 21:36:15 +0000

    Oops! Had missing semicolon in array reporter.

--- quail/reporters/reporter.array.php
+++ quail/reporters/reporter.array.php
@@ -1,5 +1,24 @@
 <?php
-
+/**
+*    QUAIL - QUAIL Accessibility Information Library
+*    Copyright (C) 2009 Kevin Miller
+*
+*    This program is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    (at your option) any later version.
+*
+*    This program is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*	@author Kevin Miller <kemiller@csumb.edu>
+*/
+/** \addtogroup reporters */
+/*@{*/
 /**
 *	An array reporter that simply returns an unformatted and nested PHP array of 
 *	tests and report objects
@@ -19,7 +38,7 @@ class reportArray extends quailReporter {
 			$translation = $this->guideline->getTranslation($testname);
 			$output[$testname]['severity'] = $this->guideline->getSeverity($testname);
 			$output[$testname]['title'] =  $translation['title'];
-			$output[$testname]['body'] = $translation['description']
+			$output[$testname]['body'] = $translation['description'];
 			foreach($test as $k => $problem) {
 				if(is_object($problem)) {
 					$output[$testname]['problems'][$k]['element'] =  htmlentities($problem->getHtml());
@@ -34,3 +53,4 @@ class reportArray extends quailReporter {
 		return $output;
 	}
 }
+/*@}*/
