cea0fb9be4d4f9fef2135b44b382c065361be6c8
keveemiller <keveemiller@0cbe4d32-2880-11de-b161-0f7b0ef88f65>
2010-06-25 18:21:48 +0000

    Fixed check for head having child nodes before being foreached over in linkUsedForAlternateContent

--- trunk/quail/common/accessibility_tests.php
+++ trunk/quail/common/accessibility_tests.php
@@ -3301,9 +3301,11 @@ class linkUsedForAlternateContent extends quailTest {
 	function check() {
 		$head = $this->getAllElements('head');
 		$head = $head[0];
-		foreach($head->childNodes as $child) {
-			if($this->propertyIsEqual($child, 'tagName', 'link') && $child->getAttribute('rel') == 'alternate')
-				return true;
+		if($head && property_exists($head, 'childNodes')) {
+			foreach($head->childNodes as $child) {
+				if($this->propertyIsEqual($child, 'tagName', 'link') && $child->getAttribute('rel') == 'alternate')
+					return true;
+			}
 		}
 		$this->addReport(null, null, false);
 	}
